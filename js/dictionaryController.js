5dbc8ff23c901b992b8b042139f98f7a47be91ff
Austin Julius Kim <austinjuliuskim@gmail.com>
2015-04-25 02:31:50 -0700

    scripts linkes controller app

---
+++ js/dictionaryController.js
@@ -0,0 +1,10 @@
+angular
+	.module('dicionaryApp')
+	.controller('DictionaryController', DictionaryController);
+
+function DictionaryController(){
+	var self = this;
+	self.test = console.log("this is a test")
+
+	
+}
